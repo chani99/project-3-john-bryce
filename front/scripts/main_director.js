f8882a4a2cfee3212000596ab7e6f05f3bdf59c2
chani <click0548450396@gmail.com>
2017-11-14 16:00:57 +0200

    admin

--- front/scripts/main_director.js
+++ front/scripts/main_director.js
@@ -28,7 +28,7 @@ var main_screen = function() {
             //get cuorse list & student list
             let column33 = new column3_director();
             column33.main_screen(function() {
-                let AdminController = new AdminModelController();
+                let AdminController = new AdminModuleController();
                 AdminController.GetAllAdmins();
                 $('#screen2').hide();
             });
@@ -48,4 +48,11 @@ var main_screen = function() {
 $('#nav_school').click(function() {
     let loadmain = new main_screen();
     loadmain.loadmaindcreen();
+});
+
+
+// add event for nav bar school
+$('#nav_Administration').click(function() {
+    let loadmain = new main_screen();
+    loadmain.loadadminscreen();
 });
