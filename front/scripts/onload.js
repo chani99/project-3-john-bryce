ee79124436ef8044ba5bf06e85309b27ee84d84e
chani <click0548450396@gmail.com>
2017-11-23 09:19:49 +0200

    sdasdasd

--- front/scripts/onload.js
+++ front/scripts/onload.js
@@ -4,20 +4,3 @@ $(document).ready(function() {
     let login = new main_screen();
     login.login_screen();
 });
-
-$(function() {
-    // Remove the # from the hash, as different browsers may or may not include it
-    var hash = location.hash.replace('#', '');
-
-    if (hash != '') {
-        // Show the hash if it's set
-        alert(hash);
-
-        // Clear the hash in the URL
-        location.hash = '';
-    }
-});
-
-$(document).bind("hashchange", function() {
-    // Anchor has changed.
-});
